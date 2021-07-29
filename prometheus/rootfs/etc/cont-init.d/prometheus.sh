3523c59b22d1365963dfac9f311fab8f5d8fe594
lenisko <10072920+lenisko@users.noreply.github.com>
2021-07-29 04:27:22 +0200

    init

---
+++ prometheus/rootfs/etc/cont-init.d/prometheus.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Prometheus
+# Configures Prometheus
+# ==============================================================================
+echo "${HASSIO_TOKEN}" > '/run/home-assistant.token'
